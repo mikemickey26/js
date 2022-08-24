d7f5c55cac0dac93e2301b66f6afaf1de308fd02
mikemickey26 <kingskyrocker@gmail.com>
2022-08-24 11:59:54 +0100

    updated

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ function multiplytwonumbers(num1,num2) {
 console.log(multiplytwonumbers(56,67));
 console.log(multiplytwonumbers(70,70));
 console.log(multiplytwonumbers(99,99));
+console.log(multiplytwonumbers(7,7));
