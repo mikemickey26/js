de6a6689ff02de6e348d682f30064689f76c140d
mikemickey26 <kingskyrocker@gmail.com>
2022-08-24 11:52:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function multiplytwonumbers(num1,num2) {
+    return num1 * num2
+}
+console.log(multiplytwonumbers(56,67));
+console.log(multiplytwonumbers(70,70));
+console.log(multiplytwonumbers(99,99));
